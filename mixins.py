453fbe6bca2eafbe3347b1f7b441d58f5d2ecec3
Cristian Carpenter <ccarpenterg@gmail.com>
2012-06-26 20:31:57 -0700

    Created the ChannelMixin class to emulate a chat with rooms.

--- mixins.py
+++ mixins.py
@@ -35,3 +35,44 @@ class MessageMixin(object):
         if len(cls.cache) > self.cache_size:
             cls.cache = cls.cache[-self.cache_size:]
 
+class ChannelMixin(object):
+    channels = dict()
+    cache_size = 200
+
+    def create_channel(self, channel):
+        cls = ChannelMixin
+        cls.channels[channel] = dict()
+        cls.channels[channel]['waiters'] = set()
+        cls.channels[channel]['cache'] = []
+        cls.channels[channel]['cache_size'] = 200
+
+    def wait_for_messages(self, callback, channel, cursor=None):
+        cls = ChannelMixin
+        if cursor:
+            index = 0
+            for i in xrange(len(cls.channels[channel]['cache'])):
+                index = len(cls.channels[channel]['cache']) - i - 1
+                if cls.channels[channel]['cache'][index]["id"] == cursor: break
+            recent = cls.channels[channel]['cache'][index + 1:]
+            if recent:
+                callback(recent)
+                return
+        cls.channels[channel]['waiters'].add(callback)
+
+    def cancel_wait(self, callback):
+        cls = ChannelMixin
+        cls.channels[channel]['waiters'].remove(callback)
+
+    def new_messages(self, channel, messages):
+        cls = ChannelMixin
+        logging.info("Sending new message to %r listeners", len(cls.waiters))
+        for callback in cls.channels[channel]['waiters']:
+            try:
+                callback(messages)
+            except:
+                logging.error("Error in waiter callback", exc_info=True)
+        cls.channels[channel]['waiters'] = set()
+        cls.channels[channel]['cache'].extend(messages)
+        if len(cls.channels[channel]['cache']) > self.cache_size:
+            cls.channels[channel]['cache'] = cls.channels[channel]['cache'][-self.cache_size:]
+
