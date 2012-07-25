e7bfafb5311fa16b3ddda6f84ca401381c6db8c5
Cristian Carpenter <ccarpenterg@gmail.com>
2012-07-25 00:16:42 -0700

    Fixed the centralized cache building.

--- loop.py
+++ loop.py
@@ -1,19 +1,19 @@
 import tornado.ioloop
 import tornado.escape
+import tornado.options
 import logging
 import redis
+import datetime
 
 from sqlalchemy.orm import scoped_session, sessionmaker
 from models import *
+from util import SuperDict
 
 db = scoped_session(sessionmaker(bind=engine))
 store = redis.Redis(host='localhost', port=6379, db=0)
 CACHE_SIZE = 200
 
 class Cache(object):
-    def __init__(self, start):
-        self.start = start
-
     @property
     def db(self):
         return db
@@ -23,18 +23,22 @@ class Cache(object):
         return store
 
     def update(self):
-        timestamp = self.timestamp or self.start
-        channels = self.db.query(Chat).filter(Chat.timestamp > timestamp).distinct('channel_id')
-        logging.info("Updating cache on %d for %d channels", timestamp, channels.count())
+        channels = self.db.query(Chat).distinct('channel_id')
+        logging.info("Updating cache for %d channels", channels.count())
         messages = SuperDict([])
         for channel in channels:
             self.store.delete('channel:cache:%s' % channel)
             for chat in self.db.query(Chat).filter(Chat.channel_id == channel.channel_id).order_by(Chat.timestamp).limit(CACHE_SIZE):
-                self.store.rpush('channel:cache:%s' % channel, tornado.escape.json_encode(chat))
+                self.store.rpush('channel:cache:%s' % channel, chat.json)
         tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(0.00001), self.update)
 
-if __name__ == '__main__':
-    cache = Cache(int(time.time()*10**6))
+def main():
+    tornado.options.parse_command_line()
+    cache = Cache()
+    print 'starting...'
     tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(0.00001), cache.update)
     tornado.ioloop.IOLoop.instance().start()
+
+if __name__ == "__main__":
+    main()
     
