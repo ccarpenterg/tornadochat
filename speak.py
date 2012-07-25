633178debfc0ab4a92128c82ec7d28489159aed9
Cristian Carpenter <ccarpenterg@gmail.com>
2012-07-24 23:33:17 -0700

    Added a command line tool to send messages to a chanel.

---
+++ speak.py
@@ -0,0 +1,41 @@
+import tornado.options
+import tornado.template
+import tornado.escape
+import uuid
+import time
+import sys
+from tornado.options import define, options
+
+from models import *
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+db = scoped_session(sessionmaker(bind=engine))
+
+sys.path.append("/home/ccarpenterg/dev/tornadochat/templates/")
+loader = tornado.template.Loader("/home/ccarpenterg/dev/tornadochat/templates/")
+
+define("channel", default=1, help="the channel to broadcast", type=int)
+define("nickname", default="Master", help="the nickname", type=str)
+define("msg", type=str)
+
+if __name__ == '__main__':
+
+    tornado.options.parse_command_line()
+
+    message = {
+        "id": str(uuid.uuid4()),
+        "from": options.nickname,
+        "body": tornado.escape.linkify(options.msg),
+    }
+
+    #message['html'] = loader.load("message.html").generate(message=message)
+    message['html'] = tornado.template.Template(
+        '<div class="message" id="m{{ message["id"] }}"><b>{{ message["from"] }}: </b>{{ message["body"] }}</div>'
+    ).generate(message=message)
+
+    chat = Chat()
+    chat.json = tornado.escape.json_encode(message)
+    chat.timestamp = int(time.time()*10**6)
+    chat.channel_id = options.channel
+    db.add(chat)
+    db.commit()
